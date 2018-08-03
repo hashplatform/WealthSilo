0584673e461ac0662745feb6df4c0c2d5c383243
wealthsilo <41845819+wealthsilo@users.noreply.github.com>
2018-08-03 21:46:15 +0600

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -35,7 +35,7 @@ then
   sudo echo "/var/swap.img none swap sw 0 0" >> /etc/fstab
   cd
 
-  wget https://github.com/wealthsilo/WealthSilo/files/2256792/Linux.zip
+  wget https://github.com/wealthsilo/WealthSilo/files/2257942/Linux-daemon.zip
   unzip Linux.zip
   chmod +x Linux/bin/*
   sudo mv  Linux/bin/* /usr/local/bin
