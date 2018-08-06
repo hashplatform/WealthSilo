7cf411acc96bd0501b99f6376d646636a1fc9748
wealth-silo <stefan.jan.sj21@gmail.com>
2018-08-06 03:17:42 -0700

    small fix from differnce of pivx

--- src/masternode-sync.cpp
+++ src/masternode-sync.cpp
@@ -27,13 +27,11 @@ CMasternodeSync::CMasternodeSync()
 
 bool CMasternodeSync::IsSynced()
 {
-return true; 
     return RequestedMasternodeAssets == MASTERNODE_SYNC_FINISHED;
 }
 
 bool CMasternodeSync::IsBlockchainSynced()
 {
-return true; // changed to assume synced
     static bool fBlockchainSynced = false;
     static int64_t lastProcess = GetTime();
 
